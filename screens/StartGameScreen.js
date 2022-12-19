28b76e29258f7fb22c58edc70bebaa4d01da9599
TaisiiaK <taisiiakompaniets@gmail.com>
2022-12-19 14:37:33 +0200

    transition to GameScreen on Confirm Button Click with entered number in the input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import { useState } from 'react';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function inputHandler(enteredText) {
@@ -23,6 +23,7 @@ function StartGameScreen() {
       );
       return;
     }
+    onPickNumber(chosenNumber);
   }
 
   return (
